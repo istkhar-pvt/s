b8d80a83ad5b044ed39a6ed0893b24a897d3aae3
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2026-01-14 19:19:49 +0530

    Update start.py

--- RessoMusic/plugins/bot/start.py
+++ RessoMusic/plugins/bot/start.py
@@ -26,6 +26,7 @@ from strings import get_string
 from RessoMusic.misc import SUDOERS
 
 
+
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
@@ -35,9 +36,8 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                photo=config.START_IMG_URL,
+                photo=random.choice(START_IMG_URL),
                 caption=_["help_1"].format(config.SUPPORT_GROUP),
-                protect_content=True,
                 reply_markup=keyboard,
             )
         if name[0:3] == "sud":
@@ -87,10 +87,9 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        UP, CPU, RAM, DISK = await bot_sys_stats()
         await message.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
+            photo=random.choice(START_IMG_URL),
+            caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
@@ -106,67 +105,11 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        photo=config.START_IMG_URL,
+        photo=random.choice(START_IMG_URL),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
-
-
-welcome_group = 2
-@app.on_message(filters.new_chat_members, group=welcome_group)
-async def welcome(client, message: Message):
-    try:
-        chat_id = message.chat.id
-        for member in message.new_chat_members:
-            buttons = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text=member.first_name,  
-                            user_id=member.id        
-                        )
-                    ]
-                ]
-            )
-
-            if isinstance(config.OWNER_ID, int): 
-                if member.id == config.OWNER_ID:
-                    owner = f"#BOT_OWNER\n\n 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐁𝐎𝐒𝐒 💗\n\n{member.mention} 𝙊𝙬𝙣𝙚𝙧 𝗢𝗳 {app.mention} 𝙟𝙪𝙨𝙩 𝙟𝙤𝙞𝙣𝙚𝙙 𝙩𝙝𝙚 𝙜𝙧𝙤𝙪𝙥 <code>{message.chat.title}</code>.\n\n𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗠𝗲 𝗛𝗲𝗿𝗲 👇🏻🤭💕\n\n┏━━━━━━━━━━━━┓\n┣★ 𝗨𝗽𝗱𝗮𝘁𝗲 -: @MoonMusic_Updates\n┣★ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 -: @world_friend_chatting_zone\n┣★ 𝗕𝗼𝘁 𝗨𝘀𝗲𝗿𝗡𝗮𝗺𝗲 -: @{app.username}\n┣★ 𝙉𝙤𝙩𝙚  -: 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗢𝗻𝗹𝘆 𝗙𝗼𝗿 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗙𝗼𝗿 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 {member.mention}."
-                    sent_message = await message.reply_text(owner, reply_markup=buttons)
-                    await asyncio.sleep(180)
-                    await sent_message.delete()  
-                    return
-
-            elif isinstance(config.OWNER_ID, (list, set)): 
-                if member.id in config.OWNER_ID:
-                    owner = f"#BOT_OWNER\n\n 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐁𝐎𝐒𝐒 💗\n\n{member.mention} 𝙊𝙬𝙣𝙚𝙧 𝗢𝗳 {app.mention} 𝙟𝙪𝙨𝙩 𝙟𝙤𝙞𝙣𝙚𝙙 𝙩𝙝𝙚 𝙜𝙧𝙤𝙪𝙥 <code>{message.chat.title}</code>.\n\n𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗠𝗲 𝗛𝗲𝗿𝗲 👇🏻🤭💕\n\n┏━━━━━━━━━━━━┓\n┣★ 𝗨𝗽𝗱𝗮𝘁𝗲 -: @MoonMusic_Updates\n┣★ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 -: @world_friend_chatting_zone\n┣★ 𝗕𝗼𝘁 𝗨𝘀𝗲𝗿𝗡𝗮𝗺𝗲 -: @{app.username}\n┣★ 𝙉𝙤𝙩𝙚  -: 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗢𝗻𝗹𝘆 𝗙𝗼𝗿 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗙𝗼𝗿 𝗠𝘆 𝗢𝘄𝗻𝗲𝗿 {member.mention}."
-                    sent_message = await message.reply_text(owner, reply_markup=buttons)
-                    await asyncio.sleep(180) 
-                    await sent_message.delete()  
-                    return
-
-            if isinstance(SUDOERS, int): 
-                if member.id == SUDOERS:
-                    AMBOT = f"#Sudo_User\n\n 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐁𝐎𝐒𝐒 💗\n\n𝗢𝗙 {app.mention} 𝗦𝗨𝗗𝗢 𝗨𝗦𝗘𝗥 {member.mention} just joined the group <code>{message.chat.title}</code>.\n\n𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗠𝗲 𝗛𝗲𝗿𝗲 👇🏻🤭💕\n\n┏━━━━━━━━━━━━┓\n┣★ 𝗨𝗽𝗱𝗮𝘁𝗲 -: @MoonMusic_Updates\n┣★ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 -: @world_friend_chatting_zone\n┣★ 𝗕𝗼𝘁 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 -: @{app.username}."
-                    sent_message = await message.reply_text(AMBOT, reply_markup=buttons)
-                    await asyncio.sleep(180) 
-                    await sent_message.delete()  
-                    return
-
-            elif isinstance(SUDOERS, (list, set)):
-                if member.id in SUDOERS:
-                    AMBOT = f"#Sudo_User\n\n 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐁𝐎𝐒𝐒 💗\n\n𝗢𝗙 {app.mention} 𝗦𝗨𝗗𝗢 𝗨𝗦𝗘𝗥 {member.mention} just joined the group <code>{message.chat.title}</code>.\n\n𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗠𝗲 𝗛𝗘𝗥𝗘 👇🏻🤭💕\n\n┏━━━━━━━━━━━━┓\n┣★ 𝗨𝗽𝗱𝗮𝘁𝗲 -: @MoonMusic_Updates\n┣★ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 -: @world_friend_chatting_zone\n┣★ 𝗕𝗢𝗧 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘 -: @{app.username}."
-                    sent_message = await message.reply_text(AMBOT, reply_markup=buttons)
-                    await asyncio.sleep(180) 
-                    await sent_message.delete()  
-                    return
-
-        return
-    except Exception as e:
-        print(f"Error in welcome handler: {e}")
-        return
-
 
 
 @app.on_message(filters.new_chat_members, group=-1)
@@ -197,7 +140,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    photo=random.choice(START_IMG_URL),
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
